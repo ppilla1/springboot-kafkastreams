43cf381c5f877a95a3b63531f05bc1c6744a3cbc
ppilla1 <informprashant@gmail.com>
2019-12-27 16:48:40 +0530

    initial commit

---
+++ src/main/java/org/ppillai/kafkastreams/SpringbootKafkastreamsApplication.java
@@ -0,0 +1,13 @@
+package org.ppillai.kafkastreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkastreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkastreamsApplication.class, args);
+	}
+
+}
